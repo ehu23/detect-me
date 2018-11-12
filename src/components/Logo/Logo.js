f2e7c25ecc1e4fa0369875c6971ab871bc63c99e
Eddie Hu <ehu23@users.noreply.github.com>
2018-11-12 03:14:29 -0800

    second attempt to fix tilting

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,8 +6,8 @@ import './Logo.css';
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2 relative" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner pa3"> <img style={{paddingTop: '4px', zIndex: '2000'}} alt='logo' src={eye}/> </div>
+            <Tilt className="Tilt br2 shadow-2 relative" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3"> <img style={{paddingTop: '4px', position: 'relative', zIndex: '2000'}} alt='logo' src={eye}/> </div>
             </Tilt>
         </div>
     );
