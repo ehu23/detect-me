0bf597cd916c12247632e14c7756049af4f7bfde
Eddie Hu <ehu23@users.noreply.github.com>
2018-12-28 21:11:52 -0800

    Add tech font. Change detection box background to hexagons. Include helpful comments.

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,18 +4,22 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
     return (
         <div>
+
+            {/*Instructions Text*/}
             <p className='f3 fw1 white bg-black-30 b--black br2 bw1 shadow-3 hover-bg-black-50 textBox mb3 pa1 ba'>
-                Present to me a human photo (publicly accessible image URL), and I'll try to locate a face!{"\n\n"}Sample URL: https://i.ytimg.com/vi/aHqckA4fBTg/maxresdefault.jpg
+                Present to me a human photo {"{ publicly accessible image URL }"}, and I'll try to pinpoint a face...
+                <br/>
+                Sample URL --- https://i.ytimg.com/vi/aHqckA4fBTg/maxresdefault.jpg
             </p>
 
-
+            {/*Image Upload Box*/}
             <div className='center'>
                 <div className='center form pa4 br3 shadow-5 z-1'>
                     <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
                     <button 
                         className='w-30 grow f3 link ph3 pv dib white bg-light-purple'
                         onClick={onPictureSubmit}
-                    >DETECT ME </button>
+                    >Detect Me </button>
                 </div>
             </div>
         </div>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,9 @@ const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2 relative" options={{ max : 0 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner pa3"> <img style={{paddingTop: '4px', position: 'relative', zIndex: '2000'}} alt='logo' src={eye}/> </div>
+                <div className="Tilt-inner pa3">
+                    <img style={{paddingTop: '4px', position: 'relative', zIndex: '2000'}} alt='logo' src={eye}/>
+                </div>
             </Tilt>
         </div>
     );
